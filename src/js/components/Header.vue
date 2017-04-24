8c3bbe0224f9b0450ec9d325defdb498ebe1c949
Justina Rajic <diwaneemac@fr.diwanee25.devwebedia.com>
2017-04-24 15:42:19 +0200

    header profile image

--- src/js/components/Header.vue
+++ src/js/components/Header.vue
@@ -4,7 +4,15 @@
 			<h1 class="logo">Photo<span>gram</span></h1>
 			<nav class="mainNav mainNav--left">
 				<router-link to="home" class="mainNav__link"><span>Home</span></router-link>|
-				<router-link to="profile" class="mainNav__link"><span v-for="result in results">{{result.name}}</span></router-link>|
+				<router-link to="profile" class="mainNav__link"><span v-for="result in results">
+					<span v-if="!imgOutput" class="mainNav__img">
+						<input type="file" name="image">
+						<img src="http://lorempixel.com/100/100"/>
+					</span>
+					<span v-else class="mainNav__img">
+						<img :src="imgOutput"/>
+					</span>
+				</span></router-link>|
 				<button type="button" name="button" @click="logOut" class="mainNav__link">Sign out</button>
 			</nav>
 			<nav class="mainNav mainNav--right">
@@ -29,6 +37,8 @@
 		this.$http.get(link)
 		.then(function(response) {
 			 this.results = response.data;
+			 var imageName = response.data.result.profile.image_url;
+			 this.imgOutput = 'http://larapi.com/public/uploads/' + imageName;
 		})
 	},
 		methods: {
